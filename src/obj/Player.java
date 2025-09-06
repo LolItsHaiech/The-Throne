f9e722db218aeda8e55dd40b2267d2c520ddb734
lolItsHaiech <seyedamin13845@gmail.com>
2025-09-06 07:08:17 +0330

    stuff

--- src/obj/Player.java
+++ src/obj/Player.java
@@ -7,6 +7,7 @@ import obj.soldier.Soldier;
 import transactions.exceptions.ItemDoesntExistException;
 import util.LinkedList;
 
+import util.MaterialCost;
 import util.Position;
 import util.map.Map;
 
@@ -29,6 +30,7 @@ public class Player implements Serializable {
     private int foodCount;
     private int wealth;
 
+    private final boolean[] weaponUnlocks;
     private final boolean[][] vision;
 
     public Player(User user, String name, Tribe tribe, Game game) {
@@ -45,6 +47,7 @@ public class Player implements Serializable {
         this.castles = new LinkedList<>();
         this.soldiers = new LinkedList<>();
         this.weapons = new Map<>();
+        this.weaponUnlocks = new boolean[Weapon.values().length];
     }
 
     public Tribe getTribe() {
@@ -115,6 +118,23 @@ public class Player implements Serializable {
         return true;
     }
 
+    public boolean spend(MaterialCost materialCost) {
+        if (this.stoneCount < materialCost.stone() ||
+                this.woodCount < materialCost.wood() ||
+                this.wealth < materialCost.wealth() ||
+                this.foodCount < materialCost.food() ||
+                this.ironCount < materialCost.iron()) {
+            return false;
+        }
+        this.stoneCount -= materialCost.stone();
+        this.woodCount -= materialCost.wood();
+        this.wealth -= materialCost.wealth();
+        this.foodCount -= materialCost.food();
+        this.ironCount -= materialCost.iron();
+        return true;
+
+    }
+
     public void increaseWood(int amount) {
         this.woodCount += amount;
     }
@@ -160,7 +180,10 @@ public class Player implements Serializable {
     }
 
     public void addWeapon(Weapon weapon, int count) {
-        int last = this.weapons.get(weapon); // if null -> 0
+        Integer last = this.weapons.get(weapon); // if null -> 0
+        if (last == null) {
+            last = 0;
+        }
         this.weapons.set(weapon, last + count);
     }
 
@@ -212,5 +235,13 @@ public class Player implements Serializable {
                 this.vision[x][y] = true;
             }
         }
+    }
+
+    public boolean[] getWeaponUnlocks() {
+        return weaponUnlocks;
+    }
+
+    public void unlockWeapon(int i) {
+        this.weaponUnlocks[i] = true;
     }
 }
